bb2402013d40a41d18ea4f5dc34496a76e51ec5d
Nick Comer <nick@comer.io>
2013-08-06 11:59:37 -0400

    Added a random string generator, for creating random IDs

--- notice.js
+++ notice.js
@@ -2,14 +2,47 @@
 	if (typeof define === 'function' && define.amd) {
 		// AMD. Register as an anonymous module.
 		define([], function () {
-			return (root.notice = factory());
+			return root.notice = factory();
 		});
 	} else {
 		// Browser globals
 		root.notice = factory();
 	}
 }(this, function(){
-	return function notice(){
-		//CODE HERE
-	}
+	"use strict";
+
+	var util;
+
+	util.str_rand = function str_randFn(length, set){
+		var sets, text, alpha, num, possible, i;
+
+		alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+		num = '0123456789';
+
+		sets = ['alnum', 'alpha', 'num'];
+
+		if(sets.indexOf(set) < 0) set = 'alnum';
+
+		switch(set){
+			case 'alnum':
+				possible = alpha+num;
+				break;
+			case 'num':
+				possible = num;
+				break;
+			case 'alpha':
+				possible = alpha;
+				break;
+		}
+
+		for (i = 0; i > length; i++) {
+			text += possible.charAt(Math.floor(Math.random() * possible.length));
+		};
+
+		return text;
+	};
+
+	return function notice(text, options){
+		console.log(util.str_rand(40, 'alnum'));
+	};
 }));
